925c598d32bc8f615c42066b97b2f4e6ca6d371d
rajsuryawanshi2005 <rajsuryawanshi897@gmail.com>
2026-03-12 12:08:09 +0530

    Final update: movie search, suggestions, pagination, and UI improvements

--- script.js
+++ script.js
@@ -1,126 +1,321 @@
-const API_KEY = "e96831a2";
+const API_KEY="e96831a2";
 
-const movieContainer = document.getElementById("movieContainer");
+const container=document.getElementById("movieContainer");
 
-async function searchMovies() {
+let currentPage=1;
+let totalPages=1;
+let currentQuery="";
 
-const searchInput = document.getElementById("searchInput").value;
+/* MOVIE LISTS */
 
-if(searchInput === ""){
-alert("Please enter a movie name");
-return;
-}
+const bollywood=[
+"Pathaan","Jawan","Animal","Tiger 3","Brahmastra",
+"Drishyam 2","Gangubai Kathiawadi","Bhool Bhulaiyaa 2",
+"Shershaah","Laal Singh Chaddha","War","Andhadhun",
+"Dangal","PK","3 Idiots","Bajrangi Bhaijaan"
+];
 
-const res = await fetch(`https://www.omdbapi.com/?s=${searchInput}&apikey=${API_KEY}`);
+const tollywood=[
+"Ala Vaikunthapurramuloo","Sarileru Neekevvaru","Pushpa",
+"RRR","Major","Sita Ramam","Bimbisara",
+"Karthikeya 2","DJ Tillu","Dasara","Baby",
+"Hi Nanna","Skanda","Agent","Custody","Spy",
+"Kalki 2898 AD","HanuMan","Tillu Square","Guntur Kaaram"
+];
 
-const data = await res.json();
+const series=[
+"Sacred Games","Mirzapur","Paatal Lok",
+"The Family Man","Delhi Crime","Asur",
+"Farzi","Scam 1992","Special OPS",
+"Criminal Justice","Breathe","Hostages",
+"Made in Heaven","Panchayat","Aspirants",
+"Kota Factory","Flames","Little Things"
+];
 
-if(data.Response === "False"){
-movieContainer.innerHTML = "<h2>No Movies Found</h2>";
-return;
-}
+/* CREATE CARD */
 
-displayMovies(data.Search);
+function createCard(movie){
 
-}
-
-function displayMovies(movies){
-
-movieContainer.innerHTML = "";
-
-movies.forEach(movie => {
-
-const card = document.createElement("div");
-
+const card=document.createElement("div");
 card.classList.add("movie-card");
 
-card.innerHTML = `
+card.innerHTML=`
 
-<img src="${movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/300"}">
+<img src="${movie.Poster}">
 
 <div class="movie-info">
 
 <h3>${movie.Title}</h3>
-
-<p>Year: ${movie.Year}</p>
+<p>${movie.Year}</p>
 
 <div class="rating">
-
-<span class="star" onclick="rateMovie(this)">★</span>
-<span class="star" onclick="rateMovie(this)">★</span>
-<span class="star" onclick="rateMovie(this)">★</span>
-<span class="star" onclick="rateMovie(this)">★</span>
-<span class="star" onclick="rateMovie(this)">★</span>
-
+<span onclick="rate(this)">★</span>
+<span onclick="rate(this)">★</span>
+<span onclick="rate(this)">★</span>
+<span onclick="rate(this)">★</span>
+<span onclick="rate(this)">★</span>
 </div>
+
+<button class="trailer-btn" onclick="watchTrailer('${movie.Title}')">Trailer</button>
 
 </div>
 
 `;
 
-card.onclick = () => openMovie(movie.imdbID);
+card.onclick=()=>openMovie(movie.imdbID);
 
-movieContainer.appendChild(card);
+container.appendChild(card);
+
+}
+
+/* FETCH MOVIE */
+
+async function fetchMovie(title){
+
+const res=await fetch(`https://www.omdbapi.com/?t=${title}&apikey=${API_KEY}`);
+const data=await res.json();
+
+if(data.Response==="True") createCard(data);
+
+}
+
+/* SECTIONS */
+
+function loadBollywood(){
+container.innerHTML="";
+bollywood.forEach(fetchMovie);
+}
+
+function loadTollywood(){
+container.innerHTML="";
+tollywood.forEach(fetchMovie);
+}
+
+function loadSeries(){
+container.innerHTML="";
+series.forEach(fetchMovie);
+}
+
+function loadRandom(){
+
+container.innerHTML="";
+
+let mix=[...bollywood,...tollywood,...series];
+
+mix.sort(()=>0.5-Math.random());
+
+mix.slice(0,20).forEach(fetchMovie);
+
+}
+
+/* SEARCH */
+
+async function searchMovies(){
+
+currentPage=1;
+currentQuery=document.getElementById("searchInput").value;
+loadSearchResults();
+
+}
+
+async function loadSearchResults(){
+
+container.innerHTML="";
+
+const res=await fetch(`https://www.omdbapi.com/?s=${currentQuery}&page=${currentPage}&apikey=${API_KEY}`);
+
+const data=await res.json();
+
+if(data.Search){
+
+data.Search.forEach(createCard);
+
+totalPages=Math.ceil(data.totalResults/10);
+
+showPages();
+
+}
+
+}
+
+/* PAGINATION */
+
+function showPages(){
+
+let pages=document.getElementById("pageNumbers");
+
+pages.innerHTML="";
+
+for(let i=1;i<=totalPages;i++){
+
+let span=document.createElement("span");
+
+span.innerText=i;
+
+span.classList.add("page-number");
+
+if(i===currentPage) span.classList.add("active");
+
+span.onclick=function(){
+
+currentPage=i;
+loadSearchResults();
+
+};
+
+pages.appendChild(span);
+
+}
+
+}
+
+function nextPage(){
+
+if(currentPage<totalPages){
+
+currentPage++;
+loadSearchResults();
+
+}
+
+}
+
+function prevPage(){
+
+if(currentPage>1){
+
+currentPage--;
+loadSearchResults();
+
+}
+
+}
+
+/* SUGGESTIONS */
+
+async function showSuggestions(){
+
+let query=document.getElementById("searchInput").value;
+
+let suggestions=document.getElementById("suggestions");
+
+if(query.length<2){
+
+suggestions.innerHTML="";
+return;
+
+}
+
+const res=await fetch(`https://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`);
+
+const data=await res.json();
+
+suggestions.innerHTML="";
+
+if(data.Search){
+
+data.Search.slice(0,5).forEach(movie=>{
+
+let div=document.createElement("div");
+
+div.classList.add("suggestion-item");
+
+div.innerText=movie.Title;
+
+div.onclick=function(){
+
+document.getElementById("searchInput").value=movie.Title;
+
+suggestions.innerHTML="";
+
+currentQuery=movie.Title;
+
+searchMovies();
+
+};
+
+suggestions.appendChild(div);
 
 });
 
 }
 
-function rateMovie(star){
-
-let stars = star.parentElement.querySelectorAll(".star");
-
-let index = [...stars].indexOf(star);
-
-stars.forEach((s,i)=>{
-
-if(i <= index){
-s.style.color="gold";
-}else{
-s.style.color="gray";
 }
 
-});
+/* TRAILER */
+
+function watchTrailer(title){
+
+window.open(`https://www.youtube.com/results?search_query=${title} trailer`);
 
 }
+
+/* RATING */
+
+function rate(star){
+
+let stars=star.parentElement.children;
+
+let index=[...stars].indexOf(star);
+
+for(let i=0;i<stars.length;i++){
+
+stars[i].style.color=i<=index?"gold":"gray";
+
+}
+
+}
+
+/* MOVIE DETAILS */
 
 async function openMovie(id){
 
-const res = await fetch(`https://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`);
+const res=await fetch(`https://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`);
 
-const data = await res.json();
+const data=await res.json();
 
-const modal = document.getElementById("movieModal");
-
-const details = document.getElementById("modalDetails");
-
-details.innerHTML = `
+document.getElementById("modalDetails").innerHTML=`
 
 <h2>${data.Title}</h2>
-
 <img src="${data.Poster}" width="200">
-
 <p><b>Genre:</b> ${data.Genre}</p>
-<p><b>Year:</b> ${data.Year}</p>
 <p><b>Actors:</b> ${data.Actors}</p>
 <p><b>Plot:</b> ${data.Plot}</p>
 
 `;
 
-modal.style.display = "block";
+document.getElementById("movieModal").style.display="block";
 
 }
 
-document.querySelector(".close").onclick = function(){
+/* CLOSE MODAL */
+
+document.querySelector(".close").onclick=function(){
+
 document.getElementById("movieModal").style.display="none";
-}
-
-window.onclick = function(event){
-
-const modal = document.getElementById("movieModal");
-
-if(event.target == modal){
-modal.style.display="none";
-}
 
 }
+
+window.onclick=function(e){
+
+let modal=document.getElementById("movieModal");
+
+if(e.target==modal) modal.style.display="none";
+
+}
+
+/* HIDE SUGGESTIONS */
+
+document.addEventListener("click",function(e){
+
+if(!e.target.closest(".search-box")){
+
+document.getElementById("suggestions").innerHTML="";
+
+}
+
+});
+
+/* DEFAULT LOAD */
+
+loadRandom();
